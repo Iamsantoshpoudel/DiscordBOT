636aa3f5aa91ff05ac2e96b02fcee30bfb93e95d
Santoshpoudel <santoshpoudel06.com.np@gmail.com>
2026-08-18 04:00:39 -0700

    RE ROLL

--- deploy-commands.js
+++
@@ -1,7 +0,0 @@
-const { REST, Routes } = require('discord.js');
-const { deployGuildCommands } = require('./src/utils/deployCommands');
-
-deployGuildCommands().catch((err) => {
-  console.error('Failed to register commands:', err);
-  process.exit(1);
-});
